f0a844174ab84ac568260392d350749e017b31c3
beltervas <98856514+Terucsa@users.noreply.github.com>
2025-08-07 22:59:14 -0600

    actualiza y elimina en proveedores

--- controllers/supplier.controller.js
+++ controllers/supplier.controller.js
@@ -1,5 +1,4 @@
 import { suppliers } from '../mocka-data/supplier.data.js';
-import {products} from "../mocka-data/product.data";
 
 const getSupplierHandler = async (req, res) => {
     try{
@@ -11,7 +10,7 @@ const getSupplierHandler = async (req, res) => {
             },
         };
         res.status(200).json(response);
-    }catch (error) {
+    } catch (error) {
         console.error(error);
         return res.status(500).json({
             message: "Error interno del servidor"
@@ -31,7 +30,7 @@ const getSupplierHandlerByParam = async (req, res) => {
         let response = {}
 
         if ( !supplier ) {
-            return res.status(404).json({ error: 'Producto no existe' });
+            return res.status(404).json({ error: 'Proveedor no existe' });
         }
 
         response = {
@@ -39,7 +38,7 @@ const getSupplierHandlerByParam = async (req, res) => {
             supplier: supplier,
         }
         res.status(200).json(response);
-    }catch (error) {
+    } catch (error) {
         console.error(error);
         return res.status(500).json({
             message: "Error interno del servidor"
@@ -67,7 +66,7 @@ const postSupplierHandler = async (req, res) => {
             return res.status(400).json(response);
         }
 
-        products.push(new_supplier);
+        suppliers.push(new_supplier);
 
         response = {
             message: "success",
@@ -76,7 +75,7 @@ const postSupplierHandler = async (req, res) => {
             }
         }
         return res.status(201).json(response);
-    }catch (error) {
+    } catch (error) {
         console.error(error);
         return res.status(500).json({
             message: "Error interno del servidor"
@@ -84,8 +83,77 @@ const postSupplierHandler = async (req, res) => {
     }
 }
 
+const putSupplierHandler = async (req, res) => {
+    try{
+        const update_supplier = req.body;
+        const supplier = suppliers.findIndex(s => s.id == update_supplier.id);
+
+        let response = {};
+        if ( supplier === -1 ) {
+            response = {
+                message: "Este proveedor no existe",
+            }
+            return res.status(404   ).json(response);
+        }
+
+        suppliers[supplier] = {
+            ...supplier[supplier],
+            ...update_supplier,
+        };
+
+        response = {
+            message: "success",
+            data: {
+                supplierId: update_supplier.id,
+            }
+        }
+        return res.status(201).json(response);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Error interno del servidor"
+        });
+    }
+}
+
+const deleteSupplierHandler = async (req, res) => {
+
+    try{
+        const id = parseInt(req.params.id);
+        const supplier = suppliers.findIndex(s => s.id == id);
+        console.log(id);
+        if (isNaN(id)) {
+            return res.status(400).json({ error: 'Ingresa un número' });
+        }
+
+        let response = {};
+        if ( supplier === -1 ) {
+            response = {
+                message: "Este proveedor no existe",
+            }
+            return res.status(404).json(response);
+        }
+
+        suppliers.splice(id-1, 1);
+
+        response = {
+            message: "success",
+            data: {
+                supplierId: id,
+            }
+        }
+        return res.status(201).json(response);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Error interno del servidor"
+        });
+    }
+}
 export {
     getSupplierHandler,
     getSupplierHandlerByParam,
     postSupplierHandler,
+    putSupplierHandler,
+    deleteSupplierHandler,
 }
--- routes/supplier.route.js
+++ routes/supplier.route.js
@@ -3,6 +3,8 @@ import {
     getSupplierHandler,
     getSupplierHandlerByParam,
     postSupplierHandler,
+    putSupplierHandler,
+    deleteSupplierHandler,
 } from "../controllers/supplier.controller.js";
 
 const router = Router();
@@ -10,4 +12,6 @@ const router = Router();
 router.get('/', getSupplierHandler);
 router.get('/:id', getSupplierHandlerByParam);
 router.post('/', postSupplierHandler);
+router.put('/', putSupplierHandler);
+router.delete('/:id', deleteSupplierHandler);
 export default router;
